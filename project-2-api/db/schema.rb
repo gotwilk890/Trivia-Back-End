839a28c7fb40e087df66efbce119b0c2e8a8c97b
gotwilk890 <dave.wilk890@gmail.com>
2015-11-10 11:28:13 -0500

    Creates question table
    Table holds id, timestamp, question, answer

--- project-2-api/db/schema.rb
+++ project-2-api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110160724) do
+ActiveRecord::Schema.define(version: 20151110162004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20151110160724) do
   end
 
   add_index "guides", ["name"], name: "index_guides_on_name", unique: true, using: :btree
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "question",   null: false
+    t.string   "answer",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
