46fb122ed24ac30a4712478731b14be4b85718e7
gotwilk890 <dave.wilk890@gmail.com>
2015-11-11 14:54:39 -0500

    Adds update function to guides controller.
    This was tested using postman and was successful in updating the name of a guide already in the database.

--- project-2-api/app/controllers/guides_controller.rb
+++ project-2-api/app/controllers/guides_controller.rb
@@ -4,7 +4,7 @@ class GuidesController < ApplicationController
   def index
     render json: current_user.guides
   end
-
+  #POST /guides
   def create
     guide = current_user.guides.create(guide_params)
     if guide.save
@@ -12,10 +12,16 @@ class GuidesController < ApplicationController
     else
       render json: guide.errors, status: :unprocessable_entity
     end
-
   end
 
+  #Patch /guides/id
   def update
+    guide = current_user.guides.find(params[:id])
+    if guide.update_attributes(guide_params)
+      render json: guide, status: :updated
+    else
+      render json: guide.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
